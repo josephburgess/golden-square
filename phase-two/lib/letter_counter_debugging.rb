487b6903b8d5afb2f200b8d0400004ee5274907e
Joe Burgess <josephburgess@gmail.com>
2022-12-09 10:10:40 +0000

    refactoring letter_counter_debugging

--- phase-two/lib/letter_counter_debugging.rb
+++ phase-two/lib/letter_counter_debugging.rb
@@ -3,12 +3,13 @@ class LetterCounter
     @text = text
   end
 
-  def calculate_most_common()
+  def calculate_most_common
     counter = Hash.new(0)
     most_common = nil
     most_common_count = 0
     @text.chars.each do |char|
       next unless is_letter?(char)
+
       counter[char] = (counter[char] || 1) + 1
       # binding.irb
       if counter[char] > most_common_count
@@ -16,17 +17,17 @@ class LetterCounter
         most_common_count = counter[char]
       end
     end
-    return [most_common_count, most_common]
+    [most_common_count, most_common]
   end
 
   private
 
   def is_letter?(letter)
-    return letter =~ /[a-z]/i
+    letter =~ /[a-z]/i
   end
 end
 
-counter = LetterCounter.new("Digital Punk")
+counter = LetterCounter.new('Digital Punk')
 p counter.calculate_most_common
 
 # Intended output:
