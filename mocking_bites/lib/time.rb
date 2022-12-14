e43f228778f48609de1b62d2ff2311009c8e3a6e
Joe Burgess <josephburgess@gmail.com>
2022-12-14 12:02:29 +0000

    refactoring

--- mocking_bites/lib/time.rb
+++ mocking_bites/lib/time.rb
@@ -1,23 +1,6 @@
-# require 'date'
-# require 'json'
-
-# class TimeError
-
-#   def error(current_time)
-#     get_server_time - current_time
-#   end
-
-#   private
-
-#   def get_server_time
-#     text_response = @requester.get(URI('https://worldtimeapi.org/api/ip'))
-#     json = JSON.parse(text_response)
-#     DateTime.parse(json['utc_datetime']).to_time
-#   end
-# end
-
 require 'date'
 require 'json'
+require 'net/http'
 
 class TimeError
   def initialize(requester)
@@ -36,3 +19,6 @@ class TimeError
     DateTime.parse(json['utc_datetime']).to_time
   end
 end
+
+time_error = TimeError.new(Net::HTTP)
+p time_error.error(Time.now)
